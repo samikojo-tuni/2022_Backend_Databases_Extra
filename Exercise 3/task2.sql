cd238e7194970c34eae456fb87000399c31458a2
Sami Kojo <sami.kojo@tuni.fi>
2022-05-11 11:23:13 +0300

    Remove:Extra folder

--- Exercise 3/task2.sql
+++ Exercise 3/task2.sql
@@ -4,9 +4,3 @@
 -- Include the count of sold orders, employee's name and title in the company to
 -- the result. Order the result according the number of sales in descending order. 
 -- Don't include employees that have not sold anything in the result.
-
-SELECT COUNT(orders.id) as sales, emp.first_name, emp.last_name, emp.job_title
-FROM orders
-INNER JOIN employees AS emp ON emp.id = orders.employee_id
-GROUP BY(emp.id)
-ORDER BY sales DESC;
